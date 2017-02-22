e910f1217ee64cb1ae25a4e296adb1ca1d4b5975
nick742 <nan.gupta742@gmail.com>
2017-02-23 01:11:56 +0530

    Create cardio.php

---
+++ cardio.php
@@ -0,0 +1,40 @@
+<html >
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+<style>
+body {
+	background-image: url("cardio1.jpeg");
+	background-repeat: no-repeat;
+	background-size: cover;
+	
+	font-size: 20px;
+	}
+table
+{
+	border-collapse:
+}
+td {
+    border:none;
+	color:white;
+}
+</style>
+</head>
+<body>
+<form action="cardio1.php" method="post" >
+<table width="300" border="1" cellpadding="5">
+  <tr><td colspan="2" align="center">Please fill in the details</td></tr>
+  
+  <tr><td>height<td>
+  <input type="number" name="height"></input>cm</td></td></tr>
+  
+  <tr><td>weight<td><input type="number" name="weight" />kg</td></td></tr>
+   <tr><td>abdomen<td><input type="number" name="ab"/>in</td></td></tr>
+    <tr><td>dietician?<td><input type="radio" name="dietician" value="yes" />YES<input type="radio" name="radio" value="no" />NO</td></td></tr>
+   <tr><td>years of experience<td><input type="number" form="formc1"name="years" /></td></td></tr>
+    <tr><td>gender<td><input type="radio" name="gender" value="M" />MALE<p><input type="radio" name="gender" value="F" />FEMALE</td></td></tr></td></td></tr>
+</table>
+<input type="submit" value="all set to go!!"align="middle"/>
+</form>
+</body>
+</html>
