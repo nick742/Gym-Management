4bc52bef0c7ea26e6c17bf26827f0b3fc6038276
nick742 <nan.gupta742@gmail.com>
2017-02-23 01:17:47 +0530

    Create intro.php

---
+++ intro.php
@@ -0,0 +1,28 @@
+
+<html >
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+<link href="title.css" rel="stylesheet" type="text/css" />
+
+</head>
+<body>
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+<table width="100" border="5" align="left" color="white">
+  <tr>
+    <th height="10" colspan="3"><h4>WELCOME!</h4></th>
+  </tr>
+  <tr> 
+   <td colspan="2">INSTRUCTOR LOGIN<b><form action="login.php" method="post"> user id: <input type="text" size="40" name="id">
+  password:<input type="password" name="password" size="40" />
+  <input type="submit" value="login" /></form>
+  </td>
+  <td colspan="1"><a href="signup.php" >New member?click here to sign up!!</a></td> 
+  </tr>
+</table>
+<div style="position: relative">
+            <p style="position: fixed; bottom: 0; width:100%; text-align: center"> <a href="mailto:jaikau@gmail.com" align="bottom">any queries?muscle pain?click here</a>            </p>
+
+</body>
+</html>
+
