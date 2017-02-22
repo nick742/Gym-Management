69c692f68fb283046342cacd1127485ed6993356
nick742 <nan.gupta742@gmail.com>
2017-02-23 01:14:31 +0530

    Create delete.php

---
+++ delete.php
@@ -0,0 +1,26 @@
+<html >
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+<style>
+body{
+	background-repeat:no-repeat;
+	background-size:cover;
+	background-image:url(de.jpg);
+}
+div{
+	font-style:oblique;
+	font-size:18px;
+	 background-color: #EF5325;
+	height: auto;
+    width: 700px;
+    padding: 25px;
+    border: 25px  groove #555;
+    margin: 10px;
+}
+</style>
+</head>
+<body>
+<div><form action="delete1.php" method="post"><h3>please enter your id </h3><input type="number" name="id" /><br /><input type="submit" value="go" />
+</form><a href="search.php">forgot your id?no problem click here!</a></div>
+</body>
