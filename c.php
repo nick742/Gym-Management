7aa7ced406789772a29b4b40d70ee9340d917770
nick742 <nan.gupta742@gmail.com>
2017-02-23 01:11:23 +0530

    Create c.php

---
+++ c.php
@@ -0,0 +1,41 @@
+<html>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+<title>Untitled Document</title>
+<link href="course.css" rel="stylesheet" type="text/css" />
+
+<head>
+<style>
+
+div {
+    background-color: lightgrey;
+	bottom: 300px;
+    width: 300px;
+    padding: 25px;
+    border: 25px solid navy;
+    margin: 25px;
+}
+</style>
+</head>
+<body>
+<div>
+<form action="formw.php" method="post"><input type="image" src="weight.jpg" /></form>
+<p>
+recently saw arnold hrithik roshan? john abraham?want to have biceps bigger than machine guns? join our 12 month weight training program for only 12000/-annum!
+</p></div>
+
+<div>
+<form action="formc.php	"  method="post"><input type="image" src="cardio.jpg" /></form><p>
+tired of sitting around at your house?feeling your legs are heavy as mountains? time to use them! join our 6 month guaranteed fat loss program which includes aerobics and cardio for 1500/- month.NOTE-female members get 40%discount on zoomba classes!</p></div>
+
+<div>
+<form action="formd.php"  method="post"><input type="image" src="diet.jpg" /></form><p>
+Gaining weight too fast? want to lose weight by minimal efforts? going through deficiencies? all solution to your dite problems can be solved here through are qualified dietician for 500 per appointment
+</p></div>
+
+<div>
+<form action="forms.php"  method="post"><input type="image" src="spa.jpg" /></form><p>
+daily stress and work loads exhausts our body to extreme levels..one of them most effective way is to have a good massage and then spa in a world class spa room and get your body relaxed from our expert thai massagers. for only 500 per hour.</p></div>
+
+</body>
+</html>
